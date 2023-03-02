c64a358d334a22b23a73071654bd1f425d4c3342
Arjen <124262203+arjenxyz@users.noreply.github.com>
2023-03-02 20:17:55 +0300

    Update eleven.js

--- eleven.js
+++ eleven.js
@@ -1,4 +1,4 @@
-import { getAnswer } from "./mainAI.js";
+import { getAnswer } from "./one.js";
 
 export async function answer(message, client) {
     var question = message.content.replace(client.user.id, "").replace("<@> ", "").trim();
@@ -8,11 +8,11 @@ export async function answer(message, client) {
         }
         
         if (question === "Kim tarafından oluşturuldun?") {
-            message.reply("**`İtalyan#0142`**\nWebsite : https://arjenxyz.vercel.app");
+            message.reply("**Developer: `İtalyan#0142`**\n**Website:** https://arjenxyz.vercel.app");
         }
         
         if (question === "Adın ne?") {
-            message.reply("Bana KÜÇÜK İTALYAN diyorlar");
+            message.reply("Herhangi bir ismim bulunmamaktadır.\nFakat sahibim İtalyan#0142 bana küçük İtalyan der..");
         }
     });
 }
